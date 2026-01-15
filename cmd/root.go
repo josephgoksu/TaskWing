6ef7d8ead7f94c028af88c7e367d10984d270a7c
Joseph Goksu <josephgoksu@gmail.com>
2026-01-15 00:40:39 +0000

    chore: bump version to v1.12.2

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ import (
 
 var (
 	// version is the application version.
-	version = "1.12.1"
+	version = "1.12.2"
 )
 
 // rootCmd represents the base command when called without any subcommands
